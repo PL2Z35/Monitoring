716a11d87f27250a233a829c84910f8b1af9c575
PL2Z3S <cplazasmendivelso@gmail.com>
2021-10-03 22:21:09 -0500

    Modulo administrador completado

---
+++ src/public/js/scripts.js
@@ -0,0 +1,95 @@
+/*!
+ * Start Bootstrap - Freelancer v7.0.4 (https://startbootstrap.com/theme/freelancer)
+ * Copyright 2013-2021 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
+ */
+//
+// Scripts
+//
+
+window.addEventListener("DOMContentLoaded", (event) => {
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector("#mainNav");
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove("navbar-shrink");
+        } else {
+            navbarCollapsible.classList.add("navbar-shrink");
+        }
+    };
+
+    // Shrink the navbar
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener("scroll", navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector("#mainNav");
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: "#mainNav",
+            offset: 72,
+        });
+    }
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector(".navbar-toggler");
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll("#navbarResponsive .nav-link")
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener("click", () => {
+            if (window.getComputedStyle(navbarToggler).display !== "none") {
+                navbarToggler.click();
+            }
+        });
+    });
+});
+
+function myFunction() {
+    // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
+
+function myFunction2() {
+    // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput2");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable2");
+    tr = table.getElementsByTagName("tr");
+
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
--- src/routes/links.js
+++ src/routes/links.js
@@ -2,8 +2,25 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../database/database');
 
-router.get('/add', (req, res) =>{
-    res.render('links/add');
+router.get('/admin', async(req, res) =>{
+    const carreras = await pool.query('select * from carrera');
+    const tipos = await pool.query('select * from tipo');
+    const usuarios = await pool.query('select idUsuario,Nombres,Apellidos,(select Tipo from tipo where usuario.Tipo_idTipo=tipo.idTipo) as Tipo,(select Nombre from carrera where usuario.Carrera_idCarrera=carrera.idCarrera) as Carrera from usuario');
+    console.log(usuarios);
+    res.render('links/admin', {carreras,tipos,usuarios});
 })
 
+router.post('/addCarrera', async(req,res) => {
+    const{Nombre}=req.body;
+    const newObject = {Nombre};
+    await pool.query('insert into carrera set ?', [newObject]);
+    res.send('resivido');
+})
+
+router.post('/addUsuario', async(req,res) => {
+    const{idUsuario,Nombres,Apellidos,Tipo_idTipo,Carrera_idCarrera}=req.body;
+    const newObject = {idUsuario,Nombres,Apellidos,Tipo_idTipo,Carrera_idCarrera};
+    await pool.query('insert into usuario set ?', [newObject]);
+    res.send('resivido');
+})
 module.exports = router;
